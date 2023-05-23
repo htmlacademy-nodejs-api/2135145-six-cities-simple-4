83bb95552ff10f66573294bcf372e7c37b6903d4
IrinaStyazhkina <irastyazhkina@gmail.com>
2023-05-23 21:02:18 +0300

    add data generating

---
+++ src/core/file-writer/tsv-file-writer.ts
@@ -0,0 +1,27 @@
+import { createWriteStream } from 'node:fs';
+import { WriteStream } from 'node:fs';
+import { FileWriterInterface } from './file-writer.interface.js';
+
+const CHUNK_SIZE = 2 ** 16; // 64KB
+export default class TsvFileWriter implements FileWriterInterface {
+  private stream: WriteStream;
+
+  constructor(public readonly filename: string) {
+    this.stream = createWriteStream(this.filename, {
+      flags: 'w',
+      encoding: 'utf-8',
+      highWaterMark: CHUNK_SIZE,
+      autoClose: true,
+    });
+  }
+
+  public async write(row: string): Promise<void> {
+    if (!this.stream.write(`${row}\n`)) {
+      return new Promise((resolve) => {
+        this.stream.once('drain', () => resolve());
+      });
+    }
+    return Promise.resolve();
+  }
+
+}
---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,10 @@
+
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  images: string[];
+  goods: string[];
+  users: string[];
+  emails: string[];
+  avatars: string[];
+}
